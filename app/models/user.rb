9f9534302d21bea0d239e8a04ee318c054df7b39
Eri Higuchi <209428143+ehcy223@users.noreply.github.com>
2025-05-22 14:31:50 +0000

    遷移先設定まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,27 @@
 class User < ApplicationRecord
+  has_one_attached :profile_image
+  has_many :books, dependent: :destroy
+  def get_profile_image(width, height)
+    if profile_image.attached?
+      profile_image.variant(resize_to_fill: [width, height]).processed
+    else
+      'default-image.webp'
+    end
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if name = conditions.delete(:name)
+      where(conditions).where(["lower(name) = ?", name.downcase]).first
+    else
+      where(conditions).first
+    end
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [:name] 
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, on: :create #登録時のみemail必須
 end

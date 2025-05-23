8e91b5ed7e73e75f0c2d71f53fc2807b992c3acf
Eri Higuchi <209428143+ehcy223@users.noreply.github.com>
2025-05-23 03:10:07 +0000

    書籍詳細ページまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,15 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to user_path(current_user), notice: "Book was successfully created."
+    else
+      @user = current_user
+      @books = @user.books
+      render 'users/show'  
+    end
   end
 
   def edit
@@ -16,4 +25,11 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end

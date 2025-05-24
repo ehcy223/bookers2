2f87ce02e3d6f59cf84939d6448856adf0b03d6d
Eri Higuchi <209428143+ehcy223@users.noreply.github.com>
2025-05-24 14:14:18 +0000

    レイアウト未完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,9 +22,16 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: 'Book updated successfully.'
+    else
+      render :edit
+    end
   end
 
   def destroy
